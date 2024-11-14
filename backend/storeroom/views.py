a15c6f896ecbebfaa1595e67030e583f6959988b
HorynAlex <horyn.oleksandr@gmail.com>
2024-11-14 17:29:57 +0200

    Check project

---
+++ backend/storeroom/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from django import render
+
+# Create your views here.
---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+const API_URL = 'http://localhost:8000';
+
+function App() {
+
+  const test = async () => {
+    const url = `${API_URL}`
+  }
+  
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
